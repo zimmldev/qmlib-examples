9ef980baa44ad203168041f21140e15ca1e84dbf
Loic Zimmermann <loiczimm@gmail.com>
2025-06-02 23:37:07 +0200

    fix coding style

--- sources/control/main.cpp
+++ sources/control/main.cpp
@@ -5,28 +5,29 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
-int main(int argc, char *argv[]) {
-  QGuiApplication app(argc, argv);
-  QGuiApplication::setApplicationName("ControlExample");
-  QGuiApplication::setOrganizationName("zimmldev");
-  QGuiApplication::setOrganizationDomain("org.zimmldev");
+int main(int argc, char *argv[])
+{
+	QGuiApplication app(argc, argv);
+	QGuiApplication::setApplicationName("ControlExample");
+	QGuiApplication::setOrganizationName("zimmldev");
+	QGuiApplication::setOrganizationDomain("org.zimmldev");
 
-  QFont font;
-  font.setPointSize(16);
-  QGuiApplication::setFont(font);
+	QFont font;
+	font.setPointSize(16);
+	QGuiApplication::setFont(font);
 
-  QQmlApplicationEngine engine;
-  const QUrl url(u"qrc:/qt/qml/Example/qml/Entry.qml"_qs);
-  engine.setInitialProperties(
-      {{"source", "qrc:/qt/qml/ControlExample/qml/main.qml"}});
-  engine.load(url);
+	QQmlApplicationEngine engine;
+	const QUrl url(u"qrc:/qt/qml/Example/qml/Entry.qml"_qs);
+	engine.setInitialProperties(
+		{{"source", "qrc:/qt/qml/ControlExample/qml/main.qml"}});
+	engine.load(url);
 
-  QObject::connect(FontUtils::get(), &FontUtils::scaleChanged, [font] {
-    QFont f = font;
-    f.setPointSizeF(f.pointSizeF() * FontUtils::get()->scale());
+	QObject::connect(FontUtils::get(), &FontUtils::scaleChanged, [font] {
+		QFont f = font;
+		f.setPointSizeF(f.pointSizeF() * FontUtils::get()->scale());
 
-    QGuiApplication::setFont(f);
-  });
+		QGuiApplication::setFont(f);
+	});
 
-  return QCoreApplication::exec();
+	return QCoreApplication::exec();
 }
